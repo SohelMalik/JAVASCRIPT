da9762d42d0d0274d6e541bb402f7487e9485290
Sohel Malik <sohelmalkinfosys@gmail.com>
2025-03-31 17:32:05 +0530

    datatypes

---
+++ 02_variables.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello!") we are using node.js, not browser
+
+console.log(3
+    +
+    3) // code readibility should high
+
+    console.log("sohel")
+
+    let name="sohel"
+    let age=19
+    let isLoggedIn= false
+    let state
+
+    // number => 2 to power 53
+    //bigInt
+    //string=> ""
+    // boolean=>true/false
+    // null=>standalone value
+    //undefined=> 
+    // symbol=>unique
+    // object
+
+    console.log(typeof undefined); //undefined
+    
+    console.log(typeof null);//object
+    
